0c6be1508bf12a325bb9b72f442a8add9ff772dc
hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2023-06-20 10:08:41 +0900

    ファイルをアップロードしてもらう仕組みを追加

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const fs = require("fs");
 const express = require("express");
+const path = require("path");
+const multer = require("multer");
+
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/pages/index.html");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+const upload = multer({ storage: storage });
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/pages/index.html"))
+);
+
+app.post("/", upload.single("file"), function (req, res) {
+  res.send(req.file.originalname + "ファイルのアップロードが完了しました。\n処理を開始します…");
+  
+  
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
