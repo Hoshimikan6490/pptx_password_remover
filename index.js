ca9c90f6ab68478d4d6e1869d84c70f3e8b5b474
hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2023-07-04 10:13:53 +0900

    bug fix

--- index.js
+++ index.js
@@ -24,13 +24,34 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/pages/index.html"))
 );
 
+function remove_p_modifyVerifier(xml) {
+  let i = 0;
+  while (1) {
+    i++;
+    if (xml.substr(i, 1) == "p") {
+      if (xml.substr(i + 2, 14) == "modifyVerifier") {
+        var first_hand = xml.substr(0, i - 1);
+
+        for (j = i; i <= 6000; j++) {
+          if (xml.substr(j, 1) == ">") {
+            var latter_hand = xml.slice(j + 1);
+            break;
+          }
+        }
+
+        return first_hand + latter_hand;
+      }
+    }
+  }
+}
+
 app.post("/", upload.single("file"), function (req, res) {
   res.send(
     req.file.originalname +
       "ファイルのアップロードが完了しました。\n処理を開始します…"
   );
 
-  fs.rename(
+  fs.renameSync(
     `public/uploads/${req.file.originalname}`,
     `public/uploads/working.zip`,
     (err) => {
@@ -40,6 +61,17 @@ app.post("/", upload.single("file"), function (req, res) {
 
   const zip = new AdmZip(path.join(__dirname, `public/uploads/working.zip`));
   zip.extractAllTo(`public/uploads/editing`, true);
+
+  let xmlString = fs.readFileSync(
+    "public/uploads/editing/ppt/presentation.xml",
+    "utf-8"
+  );
+
+  let newXmlString = remove_p_modifyVerifier(xmlString);
+
+  fs.writeFileSync(`public/uploads/editing/ppt/presentation.xml`, newXmlString);
+
+  fs.unlinkSync("public/uploads/working.zip");
 });
 
 app.listen(port, function () {
