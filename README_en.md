7a71afa3c99470c13af90fb76d944b99a4848108
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2026-04-10 10:12:58 +0900

    fix: update README files to include support for PPSX files and adjust instructions refactor: replace AdmZip with JSZip for handling PowerPoint files

--- README_en.md
+++ README_en.md
@@ -3,7 +3,7 @@
 - **[English](https://github.com/Hoshimikan6490/pptx_password_remover/blob/master/README_en.md)** ←
 
 # About this program
-This programme removes passwords placed on PowerPoint files with the extension ".pptx".  
+This programme removes passwords placed on PowerPoint files with the extension ".pptx" or ".ppsx".  
 
 # CAUTION
 This programme can be used to delete passwords against the intention of the PPTX file creator. This is not a good practice, so please ensure that the file with the password deleted is used only by the person who deleted it. Please refrain from distributing it or any other action.  
@@ -15,26 +15,25 @@ The author of this programme is not liable for any damage caused by the use of t
 3. Run the command `npm i`.
 4. Execute the command `npx nodemon index.js` or `node index.js`. 
 5. the URL will be displayed in the terminal. So, please access that url.
-6. follow the on-screen instructions to select the PPTX file and press 'GO'.
-7. the PPTX file with the password removed will be downloaded.
+6. follow the on-screen instructions to select the PPTX/PPSX file and press 'GO'.
+7. the PPTX/PPSX file with the password removed will be downloaded.
 
 # Technical information
 ## About the action of this program
 The action of the Programme is as follows.
 1. Set up a local web server with the express package.
 2. Ask the user to select a file and copy it to the working folder named "uploads".
-3. Change the extension of PPTX files in the working folder to zip.
+3. Change the extension of PPTX/PPSX files in the working folder to zip.
 4. Unzip the zip file.
 5. Remove text containing password information.
 6. Compressed as a zip file.
-7. Change extension to PPTX.
+7. Change extension to PPTX/PPSX.
 8. Make the user download it.
-9. Remove pptx file in the working folder.
+9. Remove pptx/ppsx file in the working folder.
 
 # Packages used and their purpose
-- adm-zip: To compress and decompress zip files.
+- jszip: To load the archive, update presentation.xml, and rebuild the PPTX/PPSX file.
 - express: To set up a web server.
 - express-fileupload: On the web server, to accept files.
 - fs: To edit with files that have been uploaded.
 - nodemon: To restart immediately when there are changes related to the web server.
-- util: To implement the 'wait a few seconds' bug countermeasure.
--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-const express = require("express");
-const path = require("path");
-const fileUpload = require("express-fileupload");
-const fs = require("fs");
-const AdmZip = require("adm-zip");
-const util = require("util");
-const wait = util.promisify(setTimeout);
+const express = require('express');
+const path = require('path');
+const fileUpload = require('express-fileupload');
+const fs = require('fs');
+const fsp = fs.promises;
+const JSZip = require('jszip');
 
 const app = express();
 const port = 6490;
 
+// デバッグモードの切り替え
+// trueの場合、アップロードしたパワポのファイルを削除しないようにする。
+const debugMode = false;
+
+// ファイルアップロードの設定
 app.use(
   fileUpload({
-    defCharset: "utf8",
-    defParamCharset: "utf8",
-  })
+    defCharset: 'utf8',
+    defParamCharset: 'utf8',
+  }),
 );
 
-app.use(express.static(path.join(__dirname, "public")));
+// 静的ファイルの提供
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/pages/index.html"))
+// ルートパスへのGETリクエストに対して、index.htmlを返す
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/index.html')),
 );
 
+// presentation.xmlからp:modifyVerifierを削除する関数
 function remove_p_modifyVerifier(xml) {
   let i = 0;
   while (1) {
     i++;
-    if (xml.substr(i, 1) == "p") {
-      if (xml.substr(i + 2, 14) == "modifyVerifier") {
+    if (xml.substr(i, 1) == 'p') {
+      if (xml.substr(i + 2, 14) == 'modifyVerifier') {
         var first_hand = xml.substr(0, i - 1);
 
         for (j = i; i <= 6000; j++) {
-          if (xml.substr(j, 1) == ">") {
+          if (xml.substr(j, 1) == '>') {
             var latter_hand = xml.slice(j + 1);
             break;
           }
@@ -43,62 +50,55 @@ function remove_p_modifyVerifier(xml) {
   }
 }
 
-app.post("/", async function (req, res) {
+// POSTリクエストを処理するルート
+app.post('/', async function (req, res) {
+  // ファイルがアップロードされているか確認
   if (!req.files || Object.keys(req.files).length === 0) {
-    res.status(400).send("No files were uploaded.");
+    res.status(400).send('No files were uploaded.');
     return;
   }
 
+  // アップロードされたファイルがPowerPointファイルか確認
   let file = req.files.file;
-  let uploadPath = __dirname + "/public/uploads/" + file.name;
+  const extension = path.extname(file.name).toLowerCase();
+  if (extension !== '.pptx' && extension !== '.ppsx') {
+    res.status(400).send('Only .pptx and .ppsx files are supported.');
+    return;
+  }
 
-  file.mv(uploadPath, function (err) {
-    if (err) {
-      return res.status(500).send(err);
-    }
+  // ファイルをサーバーに保存
+  const uploadPath = path.join(__dirname, 'public/uploads', file.name);
+  await file.mv(uploadPath);
+
+  // アップロードされたPowerPointのファイルを読み込み
+  const originalBuffer = await fsp.readFile(uploadPath);
+  const zip = await JSZip.loadAsync(originalBuffer, {
+    checkCRC32: false,
   });
 
-  await wait(1000);
-  fs.renameSync(
-    `public/uploads/${file.name}`,
-    `public/uploads/working.zip`,
-    (err) => {
-      if (err) throw err;
-    }
-  );
+  // presentation.xmlを取得
+  const presentationEntry = zip.file('ppt/presentation.xml');
+  if (!presentationEntry) {
+    res
+      .status(400)
+      .send('Invalid PowerPoint file: presentation.xml not found.');
+    return;
+  }
 
-  const unzip = new AdmZip(path.join(__dirname, `public/uploads/working.zip`));
-  unzip.extractAllTo(`public/uploads/editing`, true);
+  // p:modifyVerifierを削除して、presentation.xmlを更新
+  const xmlString = await presentationEntry.async('string');
+  const newXmlString = remove_p_modifyVerifier(xmlString);
+  zip.file('ppt/presentation.xml', newXmlString);
 
-  let xmlString = fs.readFileSync(
-    "public/uploads/editing/ppt/presentation.xml",
-    "utf-8"
-  );
+  // 修正されたPowerPointファイルを再構築して保存
+  const rebuiltBuffer = await zip.generateAsync({
+    type: 'nodebuffer',
+    compression: 'DEFLATE',
+    compressionOptions: { level: 6 },
+  });
+  await fsp.writeFile(uploadPath, rebuiltBuffer);
 
-  let newXmlString = remove_p_modifyVerifier(xmlString);
-
-  fs.writeFileSync(`public/uploads/editing/ppt/presentation.xml`, newXmlString);
-
-  fs.unlinkSync("public/uploads/working.zip");
-
-  const zip = new AdmZip();
-  // フォルダを追加
-  zip.addLocalFolder("public/uploads/editing");
-
-  // zipファイル書き出し
-  zip.writeZip("public/uploads/finished.zip");
-
-  fs.renameSync(
-    `public/uploads/finished.zip`,
-    `public/uploads/${file.name}`,
-    (err) => {
-      if (err) throw err;
-    }
-  );
-
-  fs.rmSync("public/uploads/editing", { recursive: true, force: true });
-
-  await wait(2000);
+  // クライアントに修正されたPowerPointファイルをダウンロードさせる
   res.download(
     path.join(__dirname, `public/uploads/${file.name}`),
     file.name,
@@ -106,9 +106,9 @@ app.post("/", async function (req, res) {
       if (err) {
         console.log(err);
       } else {
-        fs.unlinkSync(`public/uploads/${file.name}`);
+        if (!debugMode) fs.unlinkSync(`public/uploads/${file.name}`);
       }
-    }
+    },
   );
 });
 
